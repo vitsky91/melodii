fe5f82ba476f889381694aa08d7926dddfa7fc6b
Vitalii Sidorskyi <vitalii.syborskyi@xyrality.com>
2019-09-11 15:53:28 +0200

    adjusting main page
    slider + timer

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 5, 2020 15:37:25").getTime();
+var countDownDate = new Date("Oct 25, 2019 10:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
